166c965a4c778d3836a53574989fe7df99d26285
Benjamin <h8bmf@hotmail.co.uk>
2021-07-28 14:35:42 +0100

    Second commit

---
+++ OperatorsApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace OperatorsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x = 5;
+            //int y = 5;
+            //int a = x++;
+            //int b = ++y;
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+            //for(int i =1; i<=5; ++i)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //var c = 5 / 2;
+            //var d = 5.0 / 2;
+            //var e = 5 / 3;
+            //double f = 5 / 2;
+            //Console.WriteLine(f);
+
+            //var daysInAWeek = 7;
+            //var totalDaysToBirthday = 23;
+            //var weeksToBirthday = totalDaysToBirthday / daysInAWeek;
+            //var days = totalDaysToBirthday % daysInAWeek;
+            //var res = EvenOdd(22);
+
+            const int NUM_ROWS = 2;
+            const int NUM_COLS = 5;
+            bool running = true;
+            int row = 0;
+            int col = 0;
+            int spriteNo = -1;
+
+            while(running)
+            {
+                spriteNo = ++spriteNo % (NUM_COLS * NUM_ROWS);
+                row = spriteNo / NUM_ROWS;
+                col = spriteNo % NUM_COLS;
+            }
+
+        }
+        public static bool EvenOdd(int num)
+        {
+            return num % 2 == 0;
+        }
+    }
+}
---
+++ OperatorsControlFlowTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using OperatorsApp;
+
+namespace OperatorsControlFlowTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
